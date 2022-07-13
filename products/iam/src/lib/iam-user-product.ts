a8e440bf766b7d616cc483e6d2c56bcfc254a1ea
jingood2 <jingood2@gmail.com>
2022-07-13 11:42:12 +0900

    refactor: add parameter group

--- products/iam/src/lib/iam-user-product.ts
+++ products/iam/src/lib/iam-user-product.ts
@@ -1,37 +1,47 @@
-import { CfnParameter, Lazy, SecretValue } from 'aws-cdk-lib';
-import * as iam from 'aws-cdk-lib/aws-iam';
-import * as servicecatalog from 'aws-cdk-lib/aws-servicecatalog';
-import { Construct } from 'constructs';
+import { CfnParameter, Lazy, SecretValue } from "aws-cdk-lib";
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as servicecatalog from "aws-cdk-lib/aws-servicecatalog";
+import { Construct } from "constructs";
 
-export interface IAMUserPrductProps {
-}
+export interface IAMUserPrductProps {}
 
 export class IAMUserPrduct extends servicecatalog.ProductStack {
   constructor(scope: Construct, id: string, _props: IAMUserPrductProps) {
     super(scope, id);
 
+     this.templateOptions.metadata = {
+       "AWS::CloudFormation::Interface": {
+         ParameterGroups: [
+           {
+             Label: {
+               default: "Information for IAM User",
+             },
+             Parameters: ["UserName", "Password"],
+           },
+         ],
+       },
+     };
+
     const userName = new CfnParameter(this, "UserName", {
-      type: 'String',
-      default: 'johnDoe@exmaple.com',
+      type: "String",
+      default: "johnDoe@exmaple.com",
       //description: 'IAM User Name should be email address',
-      description: '이메일 주소로 IAM 유저 이름을 입력해주세요',
-      //allowedPattern: '/[^\s@]+@[^\s@]+\.[^\s@]+/',
+      description: "Please enter your company email address as your IAM username",
+      allowedPattern: "[^@]+@[^@]+.[^@]+",
     });
 
-    const password = new CfnParameter(this, 'Password', {
-      type: 'String',
-      description: '대소문자 및 특수문자 포함하여 14글자 이상 입력해주세요',
+    const password = new CfnParameter(this, "Password", {
+      type: "String",
+      description: "Please enter at least 14 characters including uppercase and lowercase letters and special characters",
     });
 
-    new iam.User(this, 'User', {
+    new iam.User(this, "User", {
       path: "/user/",
-      groups: [iam.Group.fromGroupName(this, 'GroupName', 'UserCredentialsManagementGroup')],
+      groups: [iam.Group.fromGroupName(this, "GroupName", "UserCredentialsManagementGroup")],
       userName: Lazy.string({ produce: () => userName.valueAsString }),
       password: SecretValue.unsafePlainText(Lazy.string({ produce: () => password.valueAsString })),
       //userName: 'test@gmail.com',
       passwordResetRequired: true,
     });
-
-
   }
 }
