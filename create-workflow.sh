e56b9a271d62063ad02a505c3f3324aa4f762105
jingood2 <jingood2@gmail.com>
2022-07-22 17:19:43 +0900

    refactor: 프로젝트 디렉토리 구조 변경

--- create-workflow.sh
+++ create-workflow.sh
@@ -1,16 +1,19 @@
 # read the workflow templatikkkjje
 JOB_WORKFLOW_TEMPLATE=$(cat .github/cdk-workflow-template.yaml)
 
-ROOT_DIR='./products'
+ROOT_DIR='projects'
 BUILD_ACCOUNT='037729278610'
 TARGET_DEPLOY_ACCOUNTS=('037729278610') 
 #BRANCH_NAMES=('feat' 'develop' 'main' )
 # ACCOUNT 수만큼 reusable template에 deploy-prod 추가
-for PROJECT in $(ls ${ROOT_DIR}); do
-    echo "generating workflow for products/${PROJECT}"
+for CATEGORY in $(ls ${ROOT_DIR}); do
+    echo "generating workflow for ${ROOT_DIR}/${CATEGORY}"
 
-    for ACCOUNT in ${TARGET_DEPLOY_ACCOUNTS[@]}; do
-        JOB_WORKFLOW=$(echo "${JOB_WORKFLOW_TEMPLATE}" | sed "s/{{TARGET_ACCOUNT}}/${ACCOUNT}/g" | sed "s/{{PROJECT}}/${PROJECT}/g" )
-        echo "${JOB_WORKFLOW}" > .github/workflows/${PROJECT}.yaml
+    for PROJECT in $(ls ${ROOT_DIR}/${CATEGORY}); do
+        for ACCOUNT in ${TARGET_DEPLOY_ACCOUNTS[@]}; do
+            JOB_WORKFLOW=$(echo "${JOB_WORKFLOW_TEMPLATE}" | sed "s/{{ROOT_DIR}}/${ROOT_DIR}/g" | sed "s/{{TARGET_ACCOUNT}}/${ACCOUNT}/g" | sed "s/{{PROJECT}}/${PROJECT}/g" | sed "s/{{CATEGORY}}/${CATEGORY}/g" )
+            echo "${JOB_WORKFLOW}" > .github/workflows/${CATEGORY}/${PROJECT}.yaml
+        done
     done
+
 done
