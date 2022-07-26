543686c7cae6d72728e5cde9fcbc2232e46c9a4b
Kim Jinyoung <jingood2@gmail.com>
2022-07-26 15:23:36 +0900

    Feat/firestarter issue#20 (#55)
    * feat:add ec2 launchtemplate with ASG
    
    * refactor: update environment
    
    * feat: add portfolio tagOptions
    
    * refactor: delete unused parameter
    
    * refactor: delete unused parameter
    
    * refactor: remove id for duplicated sg
    
    * feat: add ec2 launchtemplate with autoscaling

--- projects/infra/ec2-launchtemplate/src/lib/ec2-security-group-construct.ts
+++ projects/infra/ec2-launchtemplate/src/lib/ec2-security-group-construct.ts
@@ -3,12 +3,12 @@ import { Peer, Port, SecurityGroup } from 'aws-cdk-lib/aws-ec2';
 import { Construct } from 'constructs/lib/construct';
 
 export interface Ec2SecurityGroupConstructProps {
-    securityGroupId : string;
-    allowIps?: string[];
-    allowSgIds?: string[];
-    prefixList?: string[];
-    allowPort: number;
-    description?: string;
+  securityGroupId : string;
+  allowIps?: string[];
+  allowSgIds?: string[];
+  prefixList?: string[];
+  allowPort: number;
+  description?: string;
 
 }
 
@@ -17,32 +17,32 @@ export class Ec2SecurityGroupConstruct extends Construct {
     super(scope, id);
 
     const sg = SecurityGroup.fromSecurityGroupId(this, 'SecurityGroupImport', props.securityGroupId, {
-        allowAllOutbound: true,
+      allowAllOutbound: true,
     });
 
-    for(let sgId in props.allowSgIds) {
-        sg.connections.allowFrom(
-            SecurityGroup.fromSecurityGroupId(this, 'SecurityGroupLookup', sgId), Port.tcp(props.allowPort),
-            props.description);
+    for (let sgId in props.allowSgIds) {
+      sg.connections.allowFrom(
+        SecurityGroup.fromSecurityGroupId(this, 'SecurityGroupLookup', sgId), Port.tcp(props.allowPort),
+        props.description);
     }
 
-    for(let ipAddr in props.allowIps) {
-        sg.connections.allowFrom(
-            Peer.ipv4(ipAddr), Port.tcp(props.allowPort),
-            props.description);
+    for (let ipAddr in props.allowIps) {
+      sg.connections.allowFrom(
+        Peer.ipv4(ipAddr), Port.tcp(props.allowPort),
+        props.description);
     }
 
-    for(let prefix in props.prefixList) {
-        sg.connections.allowFrom(
-            Peer.prefixList(prefix), Port.tcp(props.allowPort),
-            props.description);
+    for (let prefix in props.prefixList) {
+      sg.connections.allowFrom(
+        Peer.prefixList(prefix), Port.tcp(props.allowPort),
+        props.description);
     }
 
     // cfnoutput
     new CfnOutput(this, 'CfnOutput', {
-        description: props.description,
-        value: sg.securityGroupId,
+      description: props.description,
+      value: sg.securityGroupId,
     });
-    
+
   }
 }
