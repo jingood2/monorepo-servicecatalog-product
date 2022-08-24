d55b503998537b85630d0c7d554bf9744f611866
jingood2 <jingood2@gmail.com>
2022-08-24 17:17:16 +0900

    feat: add s3 resource based policy conifg product

---
+++ projects/security/s3-resource-based-policy/src/main.ts
@@ -0,0 +1,36 @@
+import { App, Stack, StackProps } from 'aws-cdk-lib';
+import * as servicecatalog from 'aws-cdk-lib/aws-servicecatalog';
+import { Construct } from 'constructs';
+import { S3ResourceBasedPolicy } from './lib/s3-resource-based-policy';
+
+export class MyStack extends Stack {
+  constructor(scope: Construct, id: string, props: StackProps = {}) {
+    super(scope, id, props);
+
+    // define resources here...
+    new servicecatalog.CloudFormationProduct(this, 'S3ResourceBasedPolicyProduct', {
+      productName: 'Config S3 Resource based Policy Product',
+      distributor: 'jingood2@sk.com',
+      owner: 'SK Cloud Transformation Group',
+      productVersions: [
+        {
+          productVersionName: 'v1',
+          cloudFormationTemplate: servicecatalog.CloudFormationTemplate.fromProductStack(new S3ResourceBasedPolicy(this, 'S3ResourceBasedPolicy', {})),
+        },
+      ],
+    });
+  }
+}
+
+// for development, use account/region from cdk cli
+const devEnv = {
+  account: process.env.CDK_DEFAULT_ACCOUNT,
+  region: process.env.CDK_DEFAULT_REGION,
+};
+
+const app = new App();
+
+new MyStack(app, "s3-resource-based-policy", { env: devEnv, stackName: `sc-${process.env.PROJECT_NAME}-${process.env.STAGE}` });
+// new MyStack(app, 's3-resource-based-policy-prod', { env: prodEnv });
+
+app.synth();
