943a0a58309dc23d83cbaf0c9b24369c86891c1a
jingood2 <jingood2@gmail.com>
2022-08-27 00:33:39 +0900

    feat: add beanstalk product

---
+++ projects/infra/beanstalk/src/main.ts
@@ -0,0 +1,38 @@
+import { App, Stack, StackProps } from 'aws-cdk-lib';
+import * as servicecatalog from 'aws-cdk-lib/aws-servicecatalog';
+import { Construct } from 'constructs';
+import { SCProductBeanstalkDockerStack } from './lib/beanstalk-product';
+
+export class MyStack extends Stack {
+  constructor(scope: Construct, id: string, props: StackProps = {}) {
+    super(scope, id, props);
+
+    // define resources here...
+    new servicecatalog.CloudFormationProduct(this, 'sc-beanstalk-product', {
+      productName: 'beanstalk-product',
+      owner: 'SKCnC AWSTF',
+      distributor: 'SK Cloud Transformation Group',
+      description: 'IAM User SC Product',
+      productVersions: [
+        {
+          productVersionName: 'v1',
+          cloudFormationTemplate: servicecatalog.CloudFormationTemplate.fromProductStack(
+            new SCProductBeanstalkDockerStack(this, 'BeanstalkDockerProduct', {})),
+        },
+      ],
+    });
+  }
+}
+
+// for development, use account/region from cdk cli
+const devEnv = {
+  account: process.env.CDK_DEFAULT_ACCOUNT,
+  region: process.env.CDK_DEFAULT_REGION,
+};
+
+const app = new App();
+
+new MyStack(app, 'beanstalk-dev', { env: devEnv });
+// new MyStack(app, 'beanstalk-prod', { env: prodEnv });
+
+app.synth();
