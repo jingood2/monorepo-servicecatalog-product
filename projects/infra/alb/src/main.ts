70d9c2984bf8139d441ea3c150cb4d071ada6e42
Kim Jinyoung <jingood2@gmail.com>
2022-08-27 01:06:22 +0900

    feat: add alb product v1 (#107)

---
+++ projects/infra/alb/src/main.ts
@@ -0,0 +1,40 @@
+import { App, Stack, StackProps } from 'aws-cdk-lib';
+import * as servicecatalog from 'aws-cdk-lib/aws-servicecatalog';
+import { Construct } from 'constructs';
+import path from 'path';
+
+export class MyStack extends Stack {
+  constructor(scope: Construct, id: string, props: StackProps = {}) {
+    super(scope, id, props);
+
+    // define resources here...
+    new servicecatalog.CloudFormationProduct(this, 'ACMWithACMProduct', {
+      description: 'Application Load Balancer with ACM',
+      productName: 'ALB with ACM Product',
+      distributor: 'jingood2@sk.com',
+      owner: 'SK Cloud Transformation Group',
+      productVersions: [
+        {
+          productVersionName: 'v1',
+          description: 'Application Load Balancer with ACM',
+          cloudFormationTemplate: servicecatalog.CloudFormationTemplate.fromAsset(
+            path.join(__dirname, './lib/cfn-template/alb-v1.yaml'),
+          ),
+        },
+      ],
+    });
+  }
+}
+
+// for development, use account/region from cdk cli
+const devEnv = {
+  account: process.env.CDK_DEFAULT_ACCOUNT,
+  region: process.env.CDK_DEFAULT_REGION,
+};
+
+const app = new App();
+
+new MyStack(app, 'alb-product', { env: devEnv, stackName: `SC-${process.env.PROJECT_NAME}-${process.env.STAGE}` });
+// new MyStack(app, 'alb-prod', { env: prodEnv });
+
+app.synth();
