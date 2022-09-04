21fc5baf8b3aa9135799b19f5591ec5a1be1e88b
Kim Jinyoung <jingood2@gmail.com>
2022-09-05 01:16:11 +0900

    [Feat:firestarter-issue#36] add aws managed prometheus workspace product (#112)
    * feat: add aws managed prometheus workspace product
    
    * feat: add aps product workflow

---
+++ projects/observability/aps/src/lib/aps-product.ts
@@ -0,0 +1,28 @@
+import * as cdk from 'aws-cdk-lib';
+import * as servicecatalog from 'aws-cdk-lib/aws-servicecatalog';
+import * as aps from 'aws-cdk-lib/aws-aps';
+import { Construct } from 'constructs/lib/construct';
+
+export interface APSConstructProps extends cdk.StackProps {
+
+}
+
+export class APSConstruct extends servicecatalog.ProductStack {
+  constructor(scope: Construct, id: string, _props: APSConstructProps) {
+    super(scope, id );
+
+    const cfnWorkspace = new aps.CfnWorkspace(this, 'MyCfnWorkspace', /* all optional props */ {
+        alertManagerDefinition: 'alertManagerDefinition',
+        alias: 'alias',
+        tags: [{
+            key: 'key',
+            value: 'value',
+        }],
+    });
+
+
+
+
+
+  }
+}[[]]
