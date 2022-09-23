7e97a95852c3c0a899efccfcb3e0c099e8bb75d2
Kim Jinyoung <jingood2@gmail.com>
2022-09-24 03:04:50 +0900

    Feat/firestarter issue#40 (#146)
    * feat: Setting up AWS Distro for OpenTelemetry Collector in ECS Fargate
    
    * fix: build error
    
    * fix: build error

--- projects/container/ecs/src/lib/ecs-cluster-product.ts
+++ projects/container/ecs/src/lib/ecs-cluster-product.ts
@@ -128,6 +128,7 @@ export class ECSClusterProduct extends servicecatalog.ProductStack {
         'cloudwatch:PutMetricAlarm',
         'ecs:DescribeServices',
         'ecs:UpdateService',
+        'ec2:*'
       ],
     }));
 
