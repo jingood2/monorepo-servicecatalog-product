8ab7486ddc7243854741c34e040d19a9b6a9b921
Kim Jinyoung <jingood2@gmail.com>
2022-10-16 23:13:38 +0900

    Feat/firestarter issue#42 (#155)
    * feat: create eks cluster existing vpc
    
    * feat: update PublicSubnetId Parameter
    
    * feat: add eks policy to codebuild
    
    * rollback
    
    * update
    
    * update
    
    * update
    
    * update
    
    * fix package.json

--- projects/devops/cicd/.projenrc.js
+++ projects/devops/cicd/.projenrc.js
@@ -6,11 +6,8 @@ const project = new awscdk.AwsCdkTypeScriptApp({
 
   deps: [
     'yaml',
-<<<<<<< HEAD
     'randomstring',
     '@types/randomstring',
-=======
->>>>>>> 41b3ed81d55ac5bc72db55c30cf7f4ce34b51964
   ],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
