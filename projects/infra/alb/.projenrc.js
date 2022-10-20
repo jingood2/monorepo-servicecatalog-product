ac79bba51493d774cca04153347fc03436d48602
jingood2 <jingood2@gmail.com>
2022-10-20 21:44:11 +0900

    fix:alb dummy target group randomstring

--- projects/infra/alb/.projenrc.js
+++ projects/infra/alb/.projenrc.js
@@ -1,10 +1,13 @@
 const { awscdk } = require('projen');
 const project = new awscdk.AwsCdkTypeScriptApp({
-  cdkVersion: '2.1.0',
+  cdkVersion: '2.46.0',
   defaultReleaseBranch: 'main',
   name: 'alb',
 
-  // deps: [],                /* Runtime dependencies of this module. */
+  deps: [
+    'randomstring',
+    '@types/randomstring',
+  ],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
