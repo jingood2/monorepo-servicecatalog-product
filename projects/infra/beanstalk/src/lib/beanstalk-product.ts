ee33097a6fc4b0db5f57fafe621bf052d458b8c2
Kim Jinyoung <jingood2@gmail.com>
2022-10-21 00:53:26 +0900

    Feat/firestarter issue#42 (#193)
    * feat: create eks cluster existing vpc
    
    * feat: update PublicSubnetId Parameter
    
    * feat: add eks policy to codebuild
    
    * rollback
    
    * update
    
    * update
    
    * update
    
    * update
    
    * fix package.json
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update npm package
    
    * update ecs capacityprovider name
    
    * update
    
    * update container cicd
    
    * update container cicd
    
    * update
    
    * fix build error
    
    * fix build error
    
    * fix build error
    
    * update
    
    * update
    
    * update container cicd
    
    * update
    
    * feat:Add ALB v2
    
    * update parameter
    
    * update alb name
    
    * Update Beanstalk Platform version
    
    * fix: minor fix
    
    * udpate
    
    * update ContainerPort
    
    * remove file
    
    * fix:alb dummy target group randomstring
    
    * fix:alb dummy target group randomstring
    
    * fix:alb dummy target group randomstring
    
    * fix:alb dummy target group randomstring
    
    * fix:alb dummy target group randomstring
    
    * fix:alb dummy target group randomstring
    
    * fix:alb dummy target group randomstring
    
    * fix:alb dummy target group randomstring
    
    * update eb name
    
    * update eb name
    
    * update eb name
    
    * update eb name

--- projects/infra/beanstalk/src/lib/beanstalk-product.ts
+++ projects/infra/beanstalk/src/lib/beanstalk-product.ts
@@ -472,8 +472,8 @@ export class SCProductBeanstalkDockerStack extends servicecatalog.ProductStack {
     const ebEnv = new eb.CfnEnvironment(this, 'EBEnvironment', {
       // default environmentName is `develop`
       environmentName: `${projectName.valueAsString}-beanstalk-${Environment.valueAsString}-${serviceName.valueAsString}`,
-      //applicationName: `${serviceName.valueAsString}-${Environment.valueAsString}`,
-      applicationName: ebApp.applicationName || `${serviceName.valueAsString}`,
+      applicationName: `${serviceName.valueAsString}-${Environment.valueAsString}`,
+      //applicationName: ebApp.applicationName || `${serviceName.valueAsString}`,
       solutionStackName: platformType.valueAsString,
       optionSettings: option_settings,
       cnamePrefix: condResult.toString(),
