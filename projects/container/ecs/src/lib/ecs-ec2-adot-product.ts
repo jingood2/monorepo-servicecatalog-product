9da9b0ea3029c9e2139b3de54279f57627d6c417
Kim Jinyoung <jingood2@gmail.com>
2022-10-24 06:28:17 +0900

    Feat/firestarter issue#44 (#199)
    * feat: Add Cloudmap namespace to ECS Cluster
    
    * feat: Add simplename ssm parameter
    
    * feat: Add simplename ssm parameter
    
    * fix: minor build error
    
    * fix: minor build error

--- projects/container/ecs/src/lib/ecs-ec2-adot-product.ts
+++ projects/container/ecs/src/lib/ecs-ec2-adot-product.ts
@@ -329,7 +329,6 @@ export class EcsEc2ADOTProduct extends servicecatalog.ProductStack {
     //const defaultContainerSg = ec2.SecurityGroup.fromLookupByName(this, 'DefaultContainerSG', `${projectName.valueAsString}-sg-${environment.valueAsString}-default`, vpc);
     const defaultContainerSg = ec2.SecurityGroup.fromSecurityGroupId(this, 'ContainerSG', cdk.Lazy.string( { produce: () => containerSGId.valueAsString }));
 
-    /*
     const namespace = servicediscovery.PublicDnsNamespace.fromPublicDnsNamespaceAttributes(this, 'NameSpace', {
       namespaceName: ssm.StringParameter.fromStringParameterAttributes(this, 'NamespaceName',
         { parameterName: 'namespaceName' }).stringValue,
@@ -337,7 +336,7 @@ export class EcsEc2ADOTProduct extends servicecatalog.ProductStack {
         { parameterName: 'namespaceId' }).stringValue,
       namespaceArn: ssm.StringParameter.fromStringParameterAttributes(this, 'NamespaceArn',
         { parameterName: 'namespaceName' }).stringValue,
-    }); */
+    });
 
     const cluster = ecs.Cluster.fromClusterAttributes(this, 'ECsCluster', {
       clusterName: `${projectName.valueAsString}-ecs-${environment.valueAsString}`,
@@ -369,6 +368,7 @@ export class EcsEc2ADOTProduct extends servicecatalog.ProductStack {
         // Targets port TCP port 7600 `specificContainer`
         container: container,
         containerPort: containerPort.valueAsNumber,
+        cloudMapNamespace: namespace,
       },
       capacityProviderStrategies: [
         {
